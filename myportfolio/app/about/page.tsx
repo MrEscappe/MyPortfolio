e319c4f1734df490e99e537238feb99d8b770087
José Sérgio <Maverikxd1@gmail.com>
2024-08-20 18:41:52 -0300

    contact and about pages created and styled pagina de contato e sobre criadas e estilizadas

--- myportfolio/app/about/page.tsx
+++ myportfolio/app/about/page.tsx
@@ -1,5 +1,28 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
-export default function page() {
-	return <div>page</div>;
+export default function AboutPage() {
+	return (
+		<div className="min-h-[80vh] flex items-center justify-center z-10">
+			<div className="max-w-4xl">
+				<div className="flex flex-col md:flex-row items-center gap-8">
+					<div className="md:w-1/3">
+						<Image src="/img/jsergio.jpg" alt="José Sérgio" width={300} height={300} className="rounded-full" />
+					</div>
+					<div className="md:w-2/3">
+						<p className="text-lg mb-4 px-10 md:px-0">
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum omnis voluptatem sapiente molestiae maxime! Veniam sequi quae
+							culpa doloribus cum cumque dolorem illum, deleniti, deserunt impedit autem rem atque alias.
+						</p>
+						<div className="text-center pt-10">
+							<Link href="/contact" className="bg-white text-primary px-6 py-3 text-xl  hover:text-primary_hover transition-colors">
+								Contato
+							</Link>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
---
+++ myportfolio/app/contact/page.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Image from "next/image";
+
+export default function ContactPage() {
+	const formStyle =
+		"mt-1 block w-full px-3 py-2 text-black border border-gray-300 rounded-xl shadow-sm focus:outline-none focus:ring-primary focus:border-primary sm:text-sm";
+
+	return (
+		<div className="min-h-[80vh] flex items-center justify-center z-10 ">
+			<div className="container px-4 flex flex-col md:flex-row md:gap-28">
+				<form action="https://submit-form.com/SseRNJLWq" className="w-full md:w-1/2 mb-8 md:mb-0 md:-order-last">
+					<div className="mb-4">
+						<label htmlFor="name" className="block text-sm font-medium">
+							Nome
+						</label>
+						<input type="text" name="name" id="name" className={formStyle} />
+					</div>
+					<div className="mb-4">
+						<label htmlFor="email" className="block text-sm font-medium">
+							Email
+						</label>
+						<input type="email" name="email" id="email" className={formStyle} />
+					</div>
+					<div className="mb-4">
+						<label htmlFor="subject" className="block text-sm font-medium">
+							Título
+						</label>
+						<input type="text" name="subject" id="subject" className={formStyle} />
+					</div>
+					<div className="mb-4">
+						<label htmlFor="message" className="block text-sm font-medium">
+							Assunto
+						</label>
+						<textarea name="message" id="message" rows={4} className={`${formStyle} resize-none`}></textarea>
+					</div>
+					<button type="submit" className="bg-white text-primary px-6 py-2 text-sm hover:text-primary_hover transition-colors">
+						Enviar
+					</button>
+				</form>
+				<div className="w-full md:w-1/2 md:pl-8">
+					<p className="text-lg">
+						Lorem ipsum dolor sit amet. Qui iure consectetur et aliquam voluptas sit voluptatum maiores rem similique obcaecati.
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+}
